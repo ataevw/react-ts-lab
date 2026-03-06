b679d5a8eab47fe225f77aeb991254e2b46b676d
ataevw <ataevw@kommentarii-band.ru>
2026-03-06 16:51:09 +0300

    lab-001: подключение axios, замена файлов на tsx, верстка и начальная структуризация проекта

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import ComponentShowcase from './demo/ComponentShowcase.js';
+
+function App() {
+  return (
+    <div>
+      <h1>React TS Component Library</h1>
+      <ComponentShowcase />
+    </div>
+  );
+}
+
+export default App;
